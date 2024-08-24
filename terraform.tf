5c197c7a81a76899227f565a6945d239b918104f
dksin077 <dksinghajay@gmail.com>
2024-08-24 17:49:04 +0530

    terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,6 @@
 # create public instance 
 resource "aws_instance" "my_instance" {
+  count = 1
   ami = var.image_id
   instance_type = var.instance_type
   availability_zone =  "ap-south-1a"
@@ -10,6 +11,11 @@ resource "aws_instance" "my_instance" {
     key_name = "${aws_key_pair.deployer.key_name}"
     vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
     subnet_id = "${aws_subnet.public_subnet.id}"
+}
+resource "aws_ec2_instance_state" "my_state" {
+  count = length(aws_instance.my_instance)
+  instance_id = element(aws_instance.my_instance.*.id, count.index)
+  state = "running"
 }
 
 
