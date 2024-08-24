4082b005e31836f4a7b20609da6d251ffb34e486
Ajay Singh <dksinghajay@gmail.com>
2024-08-24 17:42:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,6 @@ terraform {
       version = "5.64.0"
     }
   }
-<<<<<<< HEAD
   backend "s3" {
     bucket = "aws-s3-bucket-24082024"  # bucket should be existed
     key = "terraform.tfstate"
@@ -13,11 +12,4 @@ terraform {
     dynamodb_table = "dynamodb_db_24082024" # dynamodb should be existed
   }
 }
-=======
-}
-provider "aws" {
-  region = "ap-south-1"
-  access_key = "xyz"  
-  secret_key = "abc-xyz"
-}
->>>>>>> cd96dcddd1da1c462a1f2cb0e7007e0fa4e60a0a
+
